6707c01485a1903f4bf7eb84f53f0435c9af3116
kurtsergey <kurtsergey@gmail.com>
2018-09-26 11:11:29 +0300

    Fixed incorrect window.innerWidth on iOS
    Added hack that fixes layout

--- ios/Hybro/HybroViewManager.m
+++ ios/Hybro/HybroViewManager.m
@@ -1,5 +1,6 @@
 #import <Cordova/CDVViewController.h>
 #import <React/RCTViewManager.h>
+#import <React/RCTView.h>
 
 #import "Hybro.h"
 #import "HybroViewManager.h"
@@ -13,7 +14,13 @@ RCT_EXPORT_MODULE()
 {
   CDVViewController* vc = [CDVViewController new];
   [[Hybro getRootViewController] addChildViewController:vc];
-  return vc.view;
+  
+  // hack to fix incorrect window.innerWidth
+  vc.view.frame = CGRectMake(0, 0, 0, 0);
+  RCTView *v = [[RCTView alloc] init];
+  [v addSubview:vc.view];
+  
+  return v;
 }
 
 @end
